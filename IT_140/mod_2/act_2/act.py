d6f806ad6c33ff6b85305ba9b495bdb07b0659c7
Duncan Murchison <duncan@computeranything.dev>
2024-11-14 19:47:07 -0500

    IT-140 mod_2

---
+++ IT_140/mod_2/act_2/act.py
@@ -0,0 +1,9 @@
+# Assign decoded_tweet with user_tweet, replacing any occurrence of 'TTYL' with
+# 'talk to you later'.
+
+user_tweet = input("Enter your tweet: ")
+
+decoded_tweet = user_tweet.replace('TTYL', 'talk to you later')
+
+
+
