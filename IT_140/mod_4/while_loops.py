84d09bfaf445e37c33b143cc3bb90995e5909a16
Duncan Murchison <duncan@computeranything.dev>
2024-11-24 11:54:33 -0500

    whilez

--- IT_140/mod_4/while_loops.py
+++ IT_140/mod_4/while_loops.py
@@ -5,3 +5,27 @@ while y >= x:
     y = y - x
 print(y)
 
+# num_a = int(input('Enter first positive integer: '))
+# print()
+
+# num_b = int(input('Enter second positive integer: '))
+# print()
+
+# while num_a != num_b:
+#     if num_a > num_b:
+#         num_a = num_a - num_b
+#     else:
+#         num_b = num_b - num_a
+
+# print('GCD is {}'.format(num_a))
+
+entered_value = int(input())
+
+min_value = entered_value
+
+while entered_value > 0:
+    if entered_value < min_value:
+        min_value = entered_value
+    entered_value = int(input())
+
+print('Min value:', min_value, end='')
